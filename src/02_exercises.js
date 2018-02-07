03ba7585b111563bb315af1a48866873e4d4d584
Todd Pickell <tapickell@gmail.com>
2018-02-06 20:56:05 -0800

    initial commit on chanper 4

---
+++ src/02_exercises.js
@@ -0,0 +1,234 @@
+var again = function() { console.log("\nAgain....\n")};
+var new_exercise = function() { console.log("\nLet's try something different now.\n\n\n")};
+
+var holder = "";
+for (let index = 0; index < 7; index++) {
+  holder = holder + "#";
+  // console.log(holder);
+}
+
+again();
+
+for (let index = 0; index < 7; index++) {
+  // console.log("#".repeat(index+1));
+}
+
+again();
+
+var index = 0;
+while (index < 7) {
+  // console.log("#".repeat(index+1));
+  index += 1;
+}
+
+again();
+
+[1,2,3,4,5,6,7].forEach(times => {
+  // console.log("#".repeat(times));
+});
+
+again();
+
+var range = Array.apply(null, {length: 8}).map(Number.call, Number)
+
+range.forEach(times => {
+  // console.log("#".repeat(times));
+});
+
+new_exercise();
+
+var is_fizz = function(number) {
+  return number % 3 == 0
+}
+var is_buzz = function(number) {
+  return number % 5 == 0
+}
+
+console.log("Test")
+
+var assert = function(expected, actual) {
+  if (expected === actual) {
+    console.log('.')
+  } else {
+    console.log("Assertion failed " + expected + " != " + actual);
+  }
+}
+var assert_true = function(actual) {
+  assert(true, actual)
+}
+var assert_false = function(actual) {
+  assert(false, actual)
+}
+
+assert_true(is_fizz(3))
+assert_true(is_fizz(15))
+assert_false(is_fizz(1))
+assert_false(is_fizz(5))
+assert_true(is_buzz(5))
+assert_true(is_buzz(15))
+assert_false(is_buzz(1))
+assert_false(is_buzz(3))
+
+console.log("End testing")
+
+var is_fizz = function(number) {
+  return number % 3 == 0
+}
+var is_buzz = function(number) {
+  return number % 5 == 0
+}
+
+var range = Array.apply(null, {length: 101}).map(Number.call, Number).slice(1)
+
+range.forEach(element => {
+  let str = ""
+  if (is_fizz(element)) {
+    str = str + "Fizz"
+  }
+  if (is_buzz(element)) {
+    str = str + "Buzz"
+  }
+  if (str == "") {
+    str = element
+  }
+  console.log(str)
+});
+
+again();
+
+var fizz = function(number) {
+  return (number % 3 == 0) ? "Fizz" : ""
+}
+var buzz = function(number) {
+  return (number % 5 == 0) ? "Buzz" : ""
+}
+var empty = function(str, element) {
+  return (str == "") ? element : ""
+}
+
+range.forEach(element => {
+  let str = ""
+  str = str + fizz(element)
+  str = str + buzz(element)
+  str = str + empty(str, element)
+  console.log(str)
+});
+
+new_exercise();
+
+// var board_size = 8;
+// var board_size = 12;
+var board_size = 18;
+
+var range = Array.apply(null, {length: board_size * board_size})
+                 .map(Number.call, Number)
+                 .map(board_mapper(board_size))
+                 .join('')
+
+console.log(range)
+
+function board_mapper(board_size) {
+  return function(element) {
+    if (element % (board_size + 1) == 0) {
+      return "\n"
+    } else if (element % 2 == 0) {
+      return "#"
+    } else {
+      return " "
+    }
+  }
+}
+
+function findSolution(target) {
+  function find(current, history) {
+    if (current == target) {
+      console.log("Found a way")
+      return history
+    } else if (current > target) {
+      console.log(`Nope: ${current}`)
+      return null
+    } else {
+      return find(current + 5, `(${history} + 5)`) ||
+             find(current * 3, `(${history} * 3)`)
+    }
+  }
+  return find(1, "1")
+}
+
+console.log("Find Solution 42")
+console.log(findSolution(42))
+
+new_exercise()
+
+function printFamrInventory(cows, chickens) {
+  console.log(`${String(cows).padStart(3, "0")} Cows`)
+  console.log(`${String(chickens).padStart(3, "0")} Chickens`)
+}
+printFamrInventory(7, 11)
+
+again()
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+function outputInventory(inventory) {
+  for (const type in inventory) {
+    let count = inventory[type];
+    console.log(`${String(count).padStart(3, "0")} ${capitalize(type)}`)
+  }
+}
+
+outputInventory({"cows": 23, "chickens": 11, "horses": 8, "pigs": 1})
+
+new_exercise()
+
+function min(a, b) {
+  return (a < b) ? a : b
+}
+
+console.log(min(0, 10))
+console.log(min(0, -10))
+
+new_exercise()
+
+function is_even(number) {
+  if (number == 0) {
+    return true
+  } else if (number == 1) {
+    return false
+  } else {
+    return is_even(number - 2)
+  }
+}
+
+console.log(is_even(50))
+
+again()
+
+function is_even_better(number) {
+  if (number < 2) {
+    return number == 0
+  } else {
+    return is_even(number - 2)
+  }
+}
+
+console.log(is_even_better(50))
+console.log(is_even_better(75))
+console.log(is_even_better(0))
+console.log(is_even_better(1))
+console.log(is_even_better(-1))
+
+new_exercise()
+
+function count_char(str, char) {
+  return str.length - str.split(char).join('').length
+}
+
+function count_bs(str) {
+  return count_char(str, "B")
+}
+
+console.log(count_bs("BBC"))
+console.log(count_char("kakkerlak", "k"))
---
+++ src/04_exercises.js
@@ -0,0 +1,227 @@
+
+var again = function() { console.log("\nAgain....\n")};
+var new_exercise = function() { console.log("\nLet's try something different now.\n\n\n")};
+
+var snake_case = "words_connected_by_underscores"; // used more in other languages
+var camelCase = "wordsSeperatedByCapitalLetters"; // used more in JS, Java
+// var kebab-case = "words-connected-by-hyphens"; not valid in JS
+
+var name = "Snow White";
+var the_answer = 42;
+var ready = true;
+
+var sw = name;
+var snow_w = "Snow White";
+
+console.log(name)
+console.log(sw)
+console.log(snow_w)
+
+console.log(sw === name)
+console.log(sw == name)
+console.log(snow_w === name)
+console.log(snow_w == name)
+
+
+console.log("Snow White42" == name + the_answer)
+
+var actual = "actual"
+console.log("expected" == actual)
+
+
+new_exercise()
+console.log("OOP Approach")
+
+let journal = {
+  entries: [
+  {"events":["carrot","exercise","weekend"],"squirrel":false},
+  {"events":["bread","pudding","brushed teeth","weekend","touched tree"],"squirrel":false},
+  {"events":["carrot","nachos","brushed teeth","cycling","weekend"],"squirrel":false},
+  {"events":["brussel sprouts","ice cream","brushed teeth","computer","weekend"],"squirrel":false},
+  {"events":["potatoes","candy","brushed teeth","exercise","weekend","dentist"],"squirrel":false},
+  {"events":["brussel sprouts","pudding","brushed teeth","running","weekend"],"squirrel":false},
+  {"events":["pizza","brushed teeth","computer","work","touched tree"],"squirrel":false},
+  {"events":["bread","beer","brushed teeth","cycling","work"],"squirrel":false},
+  {"events":["cauliflower","brushed teeth","work"],"squirrel":false},
+  {"events":["pizza","brushed teeth","cycling","work"],"squirrel":false},
+  {"events":["lasagna","nachos","brushed teeth","work"],"squirrel":false},
+  {"events":["brushed teeth","weekend","touched tree"],"squirrel":false},
+  {"events":["lettuce","brushed teeth","television","weekend"],"squirrel":false},
+  {"events":["spaghetti","brushed teeth","work"],"squirrel":false},
+  {"events":["brushed teeth","computer","work"],"squirrel":false},
+  {"events":["lettuce","nachos","brushed teeth","work"],"squirrel":false},
+  {"events":["carrot","brushed teeth","running","work"],"squirrel":false},
+  {"events":["brushed teeth","work"],"squirrel":false},
+  {"events":["cauliflower","reading","weekend"],"squirrel":false},
+  {"events":["bread","brushed teeth","weekend"],"squirrel":false},
+  {"events":["lasagna","brushed teeth","exercise","work"],"squirrel":false},
+  {"events":["spaghetti","brushed teeth","reading","work"],"squirrel":false},
+  {"events":["carrot","ice cream","brushed teeth","television","work"],"squirrel":false},
+  {"events":["spaghetti","nachos","work"],"squirrel":false},
+  {"events":["cauliflower","ice cream","brushed teeth","cycling","work"],"squirrel":false},
+  {"events":["spaghetti","peanuts","computer","weekend"],"squirrel":true},
+  {"events":["potatoes","ice cream","brushed teeth","computer","weekend"],"squirrel":false},
+  {"events":["potatoes","ice cream","brushed teeth","work"],"squirrel":false},
+  {"events":["peanuts","brushed teeth","running","work"],"squirrel":false},
+  {"events":["potatoes","exercise","work"],"squirrel":false},
+  {"events":["pizza","ice cream","computer","work"],"squirrel":false},
+  {"events":["lasagna","ice cream","work"],"squirrel":false},
+  {"events":["cauliflower","candy","reading","weekend"],"squirrel":false},
+  {"events":["lasagna","nachos","brushed teeth","running","weekend"],"squirrel":false},
+  {"events":["potatoes","brushed teeth","work"],"squirrel":false},
+  {"events":["carrot","work"],"squirrel":false},
+  {"events":["pizza","beer","work","dentist"],"squirrel":false},
+  {"events":["lasagna","pudding","cycling","work"],"squirrel":false},
+  {"events":["spaghetti","brushed teeth","reading","work"],"squirrel":false},
+  {"events":["spaghetti","pudding","television","weekend"],"squirrel":false},
+  {"events":["bread","brushed teeth","exercise","weekend"],"squirrel":false},
+  {"events":["lasagna","peanuts","work"],"squirrel":true},
+  {"events":["pizza","work"],"squirrel":false},
+  {"events":["potatoes","exercise","work"],"squirrel":false},
+  {"events":["brushed teeth","exercise","work"],"squirrel":false},
+  {"events":["spaghetti","brushed teeth","television","work"],"squirrel":false},
+  {"events":["pizza","cycling","weekend"],"squirrel":false},
+  {"events":["carrot","brushed teeth","weekend"],"squirrel":false},
+  {"events":["carrot","beer","brushed teeth","work"],"squirrel":false},
+  {"events":["pizza","peanuts","candy","work"],"squirrel":true},
+  {"events":["carrot","peanuts","brushed teeth","reading","work"],"squirrel":false},
+  {"events":["potatoes","peanuts","brushed teeth","work"],"squirrel":false},
+  {"events":["carrot","nachos","brushed teeth","exercise","work"],"squirrel":false},
+  {"events":["pizza","peanuts","brushed teeth","television","weekend"],"squirrel":false},
+  {"events":["lasagna","brushed teeth","cycling","weekend"],"squirrel":false},
+  {"events":["cauliflower","peanuts","brushed teeth","computer","work","touched tree"],"squirrel":false},
+  {"events":["lettuce","brushed teeth","television","work"],"squirrel":false},
+  {"events":["potatoes","brushed teeth","computer","work"],"squirrel":false},
+  {"events":["bread","candy","work"],"squirrel":false},
+  {"events":["potatoes","nachos","work"],"squirrel":false},
+  {"events":["carrot","pudding","brushed teeth","weekend"],"squirrel":false},
+  {"events":["carrot","brushed teeth","exercise","weekend","touched tree"],"squirrel":false},
+  {"events":["brussel sprouts","running","work"],"squirrel":false},
+  {"events":["brushed teeth","work"],"squirrel":false},
+  {"events":["lettuce","brushed teeth","running","work"],"squirrel":false},
+  {"events":["candy","brushed teeth","work"],"squirrel":false},
+  {"events":["brussel sprouts","brushed teeth","computer","work"],"squirrel":false},
+  {"events":["bread","brushed teeth","weekend"],"squirrel":false},
+  {"events":["cauliflower","brushed teeth","weekend"],"squirrel":false},
+  {"events":["spaghetti","candy","television","work","touched tree"],"squirrel":false},
+  {"events":["carrot","pudding","brushed teeth","work"],"squirrel":false},
+  {"events":["lettuce","brushed teeth","work"],"squirrel":false},
+  {"events":["carrot","ice cream","brushed teeth","cycling","work"],"squirrel":false},
+  {"events":["pizza","brushed teeth","work"],"squirrel":false},
+  {"events":["spaghetti","peanuts","exercise","weekend"],"squirrel":true},
+  {"events":["bread","beer","computer","weekend","touched tree"],"squirrel":false},
+  {"events":["brushed teeth","running","work"],"squirrel":false},
+  {"events":["lettuce","peanuts","brushed teeth","work","touched tree"],"squirrel":false},
+  {"events":["lasagna","brushed teeth","television","work"],"squirrel":false},
+  {"events":["cauliflower","brushed teeth","running","work"],"squirrel":false},
+  {"events":["carrot","brushed teeth","running","work"],"squirrel":false},
+  {"events":["carrot","reading","weekend"],"squirrel":false},
+  {"events":["carrot","peanuts","reading","weekend"],"squirrel":true},
+  {"events":["potatoes","brushed teeth","running","work"],"squirrel":false},
+  {"events":["lasagna","ice cream","work","touched tree"],"squirrel":false},
+  {"events":["cauliflower","peanuts","brushed teeth","cycling","work"],"squirrel":false},
+  {"events":["pizza","brushed teeth","running","work"],"squirrel":false},
+  {"events":["lettuce","brushed teeth","work"],"squirrel":false},
+  {"events":["bread","brushed teeth","television","weekend"],"squirrel":false},
+  {"events":["cauliflower","peanuts","brushed teeth","weekend"],"squirrel":false}
+  ],
+  add_entry: function (events, squirrel) {
+    this.entries.push({events, squirrel})
+  }
+}
+
+// journal.add_entry(["work", "touched tree", "pizza", "running", "television"], false)
+// journal.add_entry(["work", "touched tree", "ice cream", "cauliflower", "lasagna", "brushed teeth"], false)
+// journal.add_entry(["weekend", "cycling", "break", "peanuts", "beer"], true)
+
+function phi(table) {
+  return (table[3] * table[0] - table[2] * table[1]) /
+    Math.sqrt((table[2] + table[3]) *
+              (table[0] + table[1]) *
+              (table[1] + table[3]) *
+              (table[0] + table[2])
+             )
+}
+
+function PHI([n00, n01, n10, n11]) {
+  return (n11 * n00 - n10 * n01) /
+    Math.sqrt((n10 + n11) * (n00 + n01) *
+              (n01 + n11) * (n00 + n10))
+}
+
+console.log(phi([76,9,4,1]))
+again()
+console.log(PHI([76,9,4,1]))
+
+function table_for(event, journal) {
+  let table = [0,0,0,0]
+  for (let i = 0; i < journal.length; i++) {
+    let entry = journal[i], index = 0
+    if (entry.events.includes(event)) index += 1
+    if (entry.squirrel) index += 2
+    table[index] +=1
+  }
+  return table
+}
+
+function journal_events(journal) {
+  let events = []
+  for (let entry of journal) {
+    for (let event of entry.events) {
+      if (!events.includes(event)) {
+        events.push(event)
+      }
+    }
+  }
+  return events
+}
+
+
+for (const event of journal_events(journal.entries)) {
+  // console.log(event + ':', phi(table_for(event, journal.entries)))
+  let correlation = phi(table_for(event, journal.entries))
+  if (correlation > 0.1 || correlation < -0.1) {
+    console.log(event + ':', correlation)
+  }
+}
+
+for (const entry of journal.entries) {
+  if (entry.events.includes("peanuts") && !entry.events.includes("brushed teeth")) {
+    entry.events.push("peanut teeth")
+  }
+}
+
+console.log(phi(table_for("peanut teeth", journal.entries)))
+
+let todo_list = []
+
+function remember(list, task) {
+  return list.concat(task)
+}
+
+function get_task(list) {
+  return list.slice(0,1)
+}
+
+function remember_urget(list, task) {
+  return [task].concat(list)
+}
+
+function max(...numbers) {
+  let result = -Infinity
+  for (let number of numbers) {
+    if (number > result) { result = number }
+  }
+  return result
+}
+
+console.log(max(99,2,54,29,90,1,23))
+console.log(max(...[99,2,54,29,90,1,23]))
+
+function random_point_on_circle(radius) {
+  let angle = Math.random() * 2 * Math.PI
+  return {x: radius * Math.cos(angle),
+          y: radius * Math.sin(angle)}
+}
+
+console.log(random_point_on_circle(2))
